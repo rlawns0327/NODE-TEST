741b2d61a3095bf287a092765c854cdf36e66a5e
rlawns0327 <30451538+rlawns0327@users.noreply.github.com>
2019-07-13 17:11:32 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+var port = process.env.PORT || 3000;
+var app = http.createServer(function(request, response){
+    var url = request.url;
+    if(request.url == '/'){
+        url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+        return response.writeHead(404);
+    }
+    response.writeHead(200);
+    console.log(__dirname + url);
+    response.end('egoing : '+ url);
+
+});
+app.listen(port);
