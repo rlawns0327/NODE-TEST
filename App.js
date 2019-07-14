94aa116598ce0d275d51a996151d72581c8c69e9
rlawns0327 <30451538+rlawns0327@users.noreply.github.com>
2019-07-14 19:36:25 +0900

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,7 @@ var fs = require('fs');
 var port = process.env.PORT || 3000;
 var app = http.createServer(function(request, response){
     var url = request.url;
-    if(request.url == '/'){
-        url = '/index.html';
-    }
+    
     if(request.url == '/favicon.ico'){
         return response.writeHead(404);
     }
